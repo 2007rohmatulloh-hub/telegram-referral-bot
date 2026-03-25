5ad4d12aac7f66e87e9bfbecefe276098e4689c4
2007rohmatulloh-hub <2007rohmatulloh@gmail.com>
2026-03-25 22:52:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from aiogram.utils.deep_linking import create_start_link, decode_payload
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ChatMemberUpdated
 
 
-API_TOKEN = "8602948149:AAFDMoeNdpMnh_ZDciCs71QklLiugYpfDvE"
+API_TOKEN = "8602948149:AAHGCR5a-sCgs3wKo5d6FYUc_ZffzxQO4Ww"
 
 CHANNEL_ID = -1002973042972
 MAIN_CHANNEL_LINK = "https://t.me/+yKejphFq__MwOGUy"
